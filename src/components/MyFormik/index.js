073d8779d0e5eaa161cbe303da3ef22c1db3e400
Husen <husen@softwareseni.com>
2021-11-15 16:57:34 +0700

    render step or single form

--- src/components/MyFormik/index.js
+++ src/components/MyFormik/index.js
@@ -9,7 +9,7 @@ import {
     // useFormik
 } from 'formik';
 import * as Yup from 'yup'
-import { FormControl, InputLabel, Select, MenuItem, TextField, Radio, RadioGroup, FormControlLabel, FormLabel } from '@mui/material'
+import { Typography, FormControl, InputLabel, Select, MenuItem, TextField, Radio, RadioGroup, FormControlLabel, FormLabel, Box, Step, Stepper, StepLabel, Button, Fade } from '@mui/material'
 import DesktopDatePicker from '@mui/lab/DesktopDatePicker'
 import AdapterDateFns from '@mui/lab/AdapterDateFns'
 import LocalizationProvider from '@mui/lab/LocalizationProvider'
@@ -63,7 +63,7 @@ export const DatePicker = (props) => {
         setValue(newValue);
     };
     return (
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <LocalizationProvider dateAdapter={AdapterDateFns} {...rest}>
             <div style={{ marginTop: "20px" }}>
                 <DesktopDatePicker
                     label={label}
@@ -80,7 +80,7 @@ export const DatePicker = (props) => {
 export const RadioButton = (props) => {
     const { name, label, options, ...rest } = props
     return (
-        <FormControl component="fieldset" style={{ marginTop: "20px" }}>
+        <FormControl component="fieldset" style={{ marginTop: "20px" }} {...rest}>
             <FormLabel component="legend">{label}</FormLabel>
             <RadioGroup row aria-label="gender" name="row-radio-buttons-group">
                 <FormControlLabel value="male" control={<Radio />} label="Male" />
@@ -114,7 +114,6 @@ const MyFormik = ({ formSchema }) => {
         let _validationSchema = {};
 
         formSchema.map((schema) => {
-            console.log("schema", { type: schema.type, schema })
             _formData[schema.id] = "";
             if (schema.type === "Text") {
                 _validationSchema[schema.id] = Yup.string();
@@ -126,21 +125,7 @@ const MyFormik = ({ formSchema }) => {
             if (schema.required) {
                 _validationSchema[schema.id] = _validationSchema[schema.id].required('Required');
             }
-            // for (var key of Object.keys(schema)) {
-            //     _formData[schema.id] = "";
-            //     console.log({ key: schema[key] });
-            //     if (schema[key].type === "Text") {
-            //         _validationSchema[schema.id] = Yup.string();
-            //     } else if (schema[key].type === "Date") {
-            //         _validationSchema[schema.id] = Yup.string().email()
-            //     } else if (schema[key].type === "Select") {
-            //         _validationSchema[schema.id] = Yup.string().oneOf(schema[key].options.map(o => o.value));
-            //     }
-
-            //     if (schema[key].required) {
-            //         _validationSchema[key] = _validationSchema[key].required('Required');
-            //     }
-            // }
+            return null
         })
 
         setFormData(_formData);
@@ -193,3 +178,108 @@ const MyFormik = ({ formSchema }) => {
 }
 
 export default MyFormik
+
+
+export const MyFormikStep = ({ schema }) => {
+    console.log({ schema });
+    const [activeStep, setActiveStep] = useState(0)
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+    };
+
+    return <Box sx={{ width: '100%' }}>
+        <Stepper activeStep={activeStep}>
+            {schema.map((e, index) => {
+                // const { Content: { description, fields, name } } = e
+                return <Step key={e.id}
+                    sx={{
+                        color: '#fff',
+                        p: 0,
+                        mx: '1px',
+                        '& .MuiSvgIcon-root': {
+                            border: '2px solid #fff',
+                            borderRadius: '50%',
+                            fontSize: '30px'
+                        },
+                        '& .MuiSvgIcon-root.Mui-active': {
+                            color: '#3271D2'
+                        },
+                        '& .MuiSvgIcon-root.Mui-completed': {
+                            color: '#3271D2'
+                        },
+                        '& .MuiStepLabel-iconContainer': {
+                            p: 0,
+                            mr: '8px',
+                            backgroundColor: '#fff',
+                            borderRadius: '50%'
+                        }
+                    }}
+                >
+                    <StepLabel
+                        sx={{
+                            backgroundColor: activeStep >= index ? '#3271D2' : '#B9BFCA',
+                            p: 3,
+                            px: 25,
+                            '& .MuiStepLabel-label': {
+                                color: '#fff'
+                            },
+                            '& .MuiStepLabel-label.Mui-active': {
+                                color: '#fff'
+                            },
+                            '& .MuiStepLabel-label.Mui-completed': {
+                                color: '#fff'
+                            }
+                        }}
+                    >{e.Title}</StepLabel>
+                </Step>
+            })}
+        </Stepper>
+        <Box sx={{ display: 'flex', pt: 2 }}>
+            {schema.map((e, i) => {
+                const { Content: { description, fields, name } } = e
+                return (i === activeStep && <Fade in={i === activeStep} key={i}>
+                    <Box sx={{ border: '1px red solid', width: '100%' }}>
+                        <Typography variant="subtitle1">Step: {name}</Typography>
+                        {description && <Typography variant="caption">{description}</Typography>}
+                        <MyFormik formSchema={fields} />
+                    </Box>
+                </Fade>)
+            })}
+            {activeStep === schema.length && <Box sx={{ border: '1px red solid', width: '100%' }}>
+                <Typography sx={{ mt: 2, mb: 1 }}>
+                    All steps completed - you&apos;re finished
+                </Typography>
+                <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                    <Box sx={{ flex: '1 1 auto' }} />
+                    <Button onClick={handleReset}>Reset</Button>
+                </Box>
+            </Box>
+            }
+        </Box>
+
+        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Button
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+            >
+                Back
+            </Button>
+            <Box sx={{ flex: '1 1 auto' }} />
+
+            <Button onClick={handleNext}>
+                {activeStep === schema.length - 1 ? 'Finish' : 'Next'}
+            </Button>
+        </Box>
+    </Box>
+}
